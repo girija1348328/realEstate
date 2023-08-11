a1518dbe138534cd635488023d591359154693a0
Girija Sankar Mohanta <106576949+girija1348328@users.noreply.github.com>
2023-08-11 17:21:45 +0530

    context

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { SearchContextProvider } from './context/SearchContext';
-import { AuthContextProvider } from './context/AuthContext';
+// import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <AuthContextProvider>
-  <SearchContextProvider>
+   <SearchContextProvider>
       <App />
     </SearchContextProvider>
-  </AuthContextProvider>
-    
-
   </React.StrictMode>
 );
